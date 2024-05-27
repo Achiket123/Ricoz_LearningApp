c437abb11c869946c2ed5fb372afe1fb6695f4eb
Anurag <anuragpandeyjee200@gmail.com>
2024-05-27 10:56:20 +0530

    watch history api

--- Server/models/subject.js
+++ Server/models/subject.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Define a schema for subjects
 const subjectSchema = new mongoose.Schema({
@@ -9,11 +9,11 @@ const subjectSchema = new mongoose.Schema({
   class: {
     type: String,
     required: true,
-    enum: ['Class 1', 'Class 2', 'Class 3', 'Class 4', 'Class 5', 'Class 6', 'Class 7', 'Class 8', 'Class 9', 'Class 10', 'Class 11', 'Class 12'],
+    enum: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
   },
 });
 
 // Create a model based on the schema
-const Subject = mongoose.model('Subject', subjectSchema);
+const Subject = mongoose.model("Subject", subjectSchema);
 
 module.exports = Subject;
---
+++ Server/models/video.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const videoSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  url: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+module.exports = mongoose.model("Video", videoSchema);
